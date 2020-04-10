adc1a0ca8f2a2d5aded11b214ba08e05decdc817
Marcelo Maciel <marcelo@arch.localdomain>
2020-04-10 11:35:37 -0300

    improve MyApplication.java & add app listener

--- src/main/java/com/marxelo/MyApplication.java
+++ src/main/java/com/marxelo/MyApplication.java
@@ -1,14 +1,44 @@
 package com.marxelo;
 
-// import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Arrays;
+import java.util.HashSet;
 
-// @EnableBatchProcessing
+import com.marxelo.configuration.MyApplicationListener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+import ch.qos.logback.classic.LoggerContext;
+
 @SpringBootApplication
+@EnableAutoConfiguration
 public class MyApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(MyApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(MyApplication.class, args);
+		SpringApplication sa = new SpringApplication();
+		sa.addListeners(new MyApplicationListener());
+		sa.setSources(new HashSet<>(
+				Arrays.asList(MyApplication.class.getName())));
+		ConfigurableApplicationContext context = sa.run(args);
+		ThreadPoolTaskScheduler bean = context.getBean(ThreadPoolTaskScheduler.class);
+		LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+
+		// Aguardar o último envio de métrica
+		try {
+			Thread.sleep(15000L);
+			LOGGER.info("awake");
+		} catch (InterruptedException e) {
+
+			e.printStackTrace();
+		}
+		loggerContext.stop();
+		bean.shutdown();
 	}
 }
