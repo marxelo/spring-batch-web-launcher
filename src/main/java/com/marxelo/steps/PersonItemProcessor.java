bc47e08e220bed5bfe2472a71f912006875633f9
Marcelo Maciel <marcelo@arch.localdomain>
2020-04-11 22:30:58 -0300

    Added person writer & processor

---
+++ src/main/java/com/marxelo/steps/PersonItemProcessor.java
@@ -0,0 +1,33 @@
+package com.marxelo.steps;
+
+import com.marxelo.models.dtos.Person;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.lang.Nullable;
+
+public class PersonItemProcessor implements ItemProcessor<Person, Person> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PersonItemProcessor.class);
+
+    static Resource[] resources = new Resource[] {new ClassPathResource("pessoas.txt") };
+
+    private StepExecution stepExecution;
+
+    @Nullable
+    @Override
+    public Person process(final Person person) throws Exception {
+        final ExecutionContext executionContext = stepExecution.getExecutionContext();
+        final int resourceIndex = executionContext.getInt("MultiResourceItemReader.resourceIndex");
+        LOGGER.info("PersonItemprocessor-processing item: " + person.toString() + " coming from resource = "
+                + resources[resourceIndex + 1]);
+                System.out.println("PersonItemprocessor-processing item: " + person.toString() + " coming from resource = "
+                + resources[resourceIndex + 1]);
+        return person;
+    }
+}
---
+++ src/main/java/com/marxelo/steps/PersonItemWriter.java
@@ -0,0 +1,28 @@
+package com.marxelo.steps;
+
+import java.util.List;
+
+import com.marxelo.models.dtos.Person;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.item.ItemWriter;
+
+public class PersonItemWriter implements ItemWriter<Person> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PersonItemWriter.class);
+
+    // private Person person;
+
+    @Override
+    public void write(List<? extends Person> items) throws Exception {
+        if (!items.isEmpty()) {
+            for (final Person person : items) {
+                System.out.println("writing item..: " + person);
+                LOGGER.info("writing item..: " + person);
+            }
+        }
+
+    }
+
+}
