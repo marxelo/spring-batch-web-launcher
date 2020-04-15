cb2b84bf553bb1a13dd3b3334895b7f9d93997b5
Marcelo Maciel <marcelo@arch.localdomain>
2020-04-14 23:21:20 -0300

    Get fileDate from chunkContest

--- src/main/java/com/marxelo/steps/tasklets/DownloadFileTasklet.java
+++ src/main/java/com/marxelo/steps/tasklets/DownloadFileTasklet.java
@@ -3,24 +3,17 @@ package com.marxelo.steps.tasklets;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@StepScope
 public class DownloadFileTasklet implements Tasklet {
     private static final Logger LOGGER = LoggerFactory.getLogger(DownloadFileTasklet.class);
-
-    @Value("#{jobParameters['fileDate']}")
-    private String fileDate;
 
     @Override
     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext)
             throws Exception {
+        String fileDate = (String) chunkContext.getStepContext().getJobParameters().get("fileDate");
         LOGGER.info("File Date in tasklet............: " + fileDate);
         System.out.println("File Date in tasklet............: " + fileDate);
         return RepeatStatus.FINISHED;
