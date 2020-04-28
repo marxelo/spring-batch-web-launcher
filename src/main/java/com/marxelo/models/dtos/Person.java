122100b1e4a20dceb0257adf406cd5d536591fe1
Marcelo Maciel <marcelo@arch.localdomain>
2020-04-28 20:45:26 -0300

    redo person.toString

--- src/main/java/com/marxelo/models/dtos/Person.java
+++ src/main/java/com/marxelo/models/dtos/Person.java
@@ -25,10 +25,33 @@ public class Person {
      * @see java.lang.Object#toString()
      */
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#toString()
+     */
+
     @Override
     public String toString() {
-        return "Person [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address.getStreetName()
-                + " " + address.getAddressNumber() + ", profession=" + profession.getProfession() + "]";
+        StringBuilder builder = new StringBuilder();
+        builder.append("Person [");
+        if (address != null) {
+            builder.append("address=").append(address.getAddressNumber()).append(", ");
+        }
+        if (firstName != null) {
+            builder.append("firstName=").append(firstName).append(", ");
+        }
+        if (lastName != null) {
+            builder.append("lastName=").append(lastName).append(", ");
+        }
+        if (lineType != null) {
+            builder.append("lineType=").append(lineType).append(", ");
+        }
+        if (profession != null) {
+            builder.append("profession=").append(profession.getProfession());
+        }
+        builder.append("]");
+        return builder.toString();
     }
 
 }
